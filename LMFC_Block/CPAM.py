0c0e13b0adadf72ca48188e4e1edff89a51ddfd0
Tianyu-Li-S <Li_ty1482916370@163.com>
2024-11-10 17:10:15 +0800

    first commit

---
+++ LMFC_Block/CPAM.py
@@ -0,0 +1,76 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.checkpoint as checkpoint
+import math
+import numpy as np
+from functools import partial
+from typing import Optional, Callable, Optional, Dict, Union
+from einops import rearrange
+from collections import OrderedDict
+from timm.layers import trunc_normal_
+from timm.layers import DropPath
+
+__all__ = ['CPAM']
+
+class channel_att(nn.Module):
+    def __init__(self, channel, b=1, gamma=2):
+        super(channel_att, self).__init__()
+        kernel_size = int(abs((math.log(channel, 2) + b) / gamma))
+        kernel_size = kernel_size if kernel_size % 2 else kernel_size + 1
+        
+        self.avg_pool = nn.AdaptiveAvgPool2d(1)
+        self.conv = nn.Conv1d(1, 1, kernel_size=kernel_size, padding=(kernel_size - 1) // 2, bias=False) 
+        self.sigmoid = nn.Sigmoid()
+
+    def forward(self, x):
+        y = self.avg_pool(x)
+        y = y.squeeze(-1)
+        y = y.transpose(-1, -2)
+        y = self.conv(y).transpose(-1, -2).unsqueeze(-1)
+        y = self.sigmoid(y)
+        return x * y.expand_as(x)
+    
+class local_att(nn.Module):
+    def __init__(self, channel, reduction=16):
+        super(local_att, self).__init__()
+        
+        self.conv_1x1 = nn.Conv2d(in_channels=channel, out_channels=channel//reduction, kernel_size=1, stride=1, bias=False)
+ 
+        self.relu   = nn.ReLU()
+        self.bn     = nn.BatchNorm2d(channel//reduction)
+ 
+        self.F_h = nn.Conv2d(in_channels=channel//reduction, out_channels=channel, kernel_size=1, stride=1, bias=False)
+        self.F_w = nn.Conv2d(in_channels=channel//reduction, out_channels=channel, kernel_size=1, stride=1, bias=False)
+ 
+        self.sigmoid_h = nn.Sigmoid()
+        self.sigmoid_w = nn.Sigmoid()
+ 
+    def forward(self, x):
+        _, _, h, w = x.size()
+        
+        x_h = torch.mean(x, dim = 3, keepdim = True).permute(0, 1, 3, 2)
+        x_w = torch.mean(x, dim = 2, keepdim = True)
+ 
+        x_cat_conv_relu = self.relu(self.bn(self.conv_1x1(torch.cat((x_h, x_w), 3))))
+ 
+        x_cat_conv_split_h, x_cat_conv_split_w = x_cat_conv_relu.split([h, w], 3)
+ 
+        s_h = self.sigmoid_h(self.F_h(x_cat_conv_split_h.permute(0, 1, 3, 2)))
+        s_w = self.sigmoid_w(self.F_w(x_cat_conv_split_w))
+ 
+        out = x * s_h.expand_as(x) * s_w.expand_as(x)
+        return out
+
+class CPAM(nn.Module):
+    # Concatenate a list of tensors along dimension
+    def __init__(self, ch=256):
+        super().__init__()
+        self.channel_att = channel_atten(ch)
+        self.local_att = local_atten(ch)
+    def forward(self, x):
+        input1,input2 = x[0], x[1]
+        input1 = self.channel_atten(input1)
+        x = input1 + input2
+        x = self.local_atten(x)
+        return x
---
+++ LMFC_Block/XConcat.py
@@ -0,0 +1,26 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import torch.utils.checkpoint as checkpoint
+import math
+import numpy as np
+from functools import partial
+from typing import Optional, Callable, Optional, Dict, Union
+from einops import rearrange
+from collections import OrderedDict
+from timm.layers import trunc_normal_
+from timm.layers import DropPath
+
+__all__ = ['XConcat']
+
+class XConcat(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def forward(self, x):
+        l, m, s = x[0], x[1], x[2]
+        tgt_size = m.shape[2:]
+        l = F.adaptive_max_pool2d(l, tgt_size) + F.adaptive_avg_pool2d(l, tgt_size)
+        s = F.interpolate(s, m.shape[2:], mode='nearest')
+        lms = torch.cat([l, m, s], dim=1)
+        return lms
